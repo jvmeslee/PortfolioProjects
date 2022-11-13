d5b2e266c639b0d71a13db35bb251ae8cf30a5c1
jvmeslee <118094560+jvmeslee@users.noreply.github.com>
2022-11-12 21:29:47 -0800

    Update COVID Project.sql

--- COVID Project.sql
+++ COVID Project.sql
@@ -3,9 +3,9 @@ From PortfolioProject..CovidDeaths$
 Where continent is not null
 order by 3,4
 
---Select *
---From PortfolioProject..CovidVaccinations$
---order by 3,4
+Select *
+From PortfolioProject..CovidVaccinations$
+order by 3,4
 
 -- Select Data that we are going to be using
 Select Location, date, total_cases, new_cases, total_deaths, population
@@ -74,7 +74,6 @@ Where dea.continent is not null
 order by 1,2,3
 
 --USE CTE
-
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -117,12 +116,7 @@ where dea.continent is not null
 Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
 
-
-
-
-
 --creating view to store data for later visualizations
-
 Create View PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
